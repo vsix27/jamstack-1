54bf4fa4759b8515fa05b8637b3e135503f1d324
Tara Z. Manicsic <tzmanics@gmail.com>
2020-03-22 02:03:42 -0400

    adds navigation files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { MatIconModule } from "@angular/material/icon";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { NavigationModule } from "./navigation/navigation.module";
-import { MatIconModule } from "@angular/material/icon";
+import { NavigationComponent } from "./navigation/navigation.component";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NavigationModule, MatIconModule],
+  declarations: [AppComponent, NavigationComponent],
+  imports: [BrowserModule, AppRoutingModule, MatIconModule],
   providers: [],
   bootstrap: [AppComponent]
 })
